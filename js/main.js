bebcef39f4e92320c2a7adcf8278027dab77fe2f
Alasdair Stalker <alasdair@asta.org.uk>
2015-03-27 23:16:03 +0000

    Add the expanded sections with scroll clicks

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+  $(function() {
+
+    $('a[href*=#]').click(function() {
+      var target = $(this.hash);
+       if (target.length) {
+         $('html,body').animate({
+             scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    });
+
+  });
+})(jQuery);
